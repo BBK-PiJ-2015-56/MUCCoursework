747965cf9eed90677839e0b11ac66bc55aeac4bd
Pete <pdomok01@mail.bbk.ac.uk>
2017-05-04 16:18:18 +0100

    created layout for long and lat values

--- app/src/main/java/peterdomokos/muccoursework/MainActivity.java
+++ app/src/main/java/peterdomokos/muccoursework/MainActivity.java
@@ -5,17 +5,22 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
 
 public class MainActivity extends AppCompatActivity {
-
-    // give code permissions an arbitrary value
+    // give runtime code permissions an arbitrary value
     private final int CODE_PERMISSIONS = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //list of permissions that will be required
+        //ask for permissions at run time
         String[] neededPermissions = {
                 Manifest.permission.CHANGE_WIFI_STATE,
                 Manifest.permission.ACCESS_WIFI_STATE,
