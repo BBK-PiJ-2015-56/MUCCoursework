fc9b7498e11dfd14d5c234f32a338c05dc05c36d
Pete <pdomok01@mail.bbk.ac.uk>
2017-05-08 12:17:54 +0100

    made instrumented unit tests for distance comparison

---
+++ app/src/androidTest/java/peterdomokos/muccoursework/IADistanceComparatorTest.java
@@ -0,0 +1,72 @@
+package peterdomokos.muccoursework;
+
+        import android.location.Location;
+
+        import android.support.test.runner.AndroidJUnit4;
+
+        import com.indooratlas.android.sdk.IALocation;
+        
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+
+        import static org.junit.Assert.assertFalse;
+        import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Pete Domokos on 08/05/2017.
+ */
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class IADistanceComparatorTest {
+
+    private IALocation mockCurrentLocation;
+    private final int RANGE = 3;
+
+    //init the mockTargetLocation
+    private Location mockTargetLocation  = new Location("aProvider");
+
+    @Test
+    public void isWithinRangeReturnsTrueWhenDistanceLessThanRange() throws Exception {
+        //set target
+        mockTargetLocation.setLongitude(-0.1299845104014142);
+        mockTargetLocation.setLatitude(51.52166396722949);
+        //set current
+        IALocation.Builder builder = new IALocation.Builder();
+        IALocation.Builder bLong = builder.withLongitude(-0.1299845104014142);
+        IALocation.Builder bLongAndLat = bLong.withLatitude(51.0);
+        mockCurrentLocation = bLongAndLat.build();
+        //check
+        assertTrue(IADistanceComparator.isWithinRange(mockCurrentLocation, mockTargetLocation, RANGE));
+    }
+    @Test
+    public void isWithinRangeReturnsFalseWhenDistanceGreaterThan3() throws Exception {
+        //set target
+        mockTargetLocation.setLongitude(-0.1299845104014142);
+        mockTargetLocation.setLatitude(51.52166396722949);
+        //set current
+        IALocation.Builder builder = new IALocation.Builder();
+        IALocation.Builder bLong = builder.withLongitude(-0.1299845104014142);
+        IALocation.Builder bLongAndLat = bLong.withLatitude(30.0);
+        mockCurrentLocation = bLongAndLat.build();
+        //check
+        assertFalse(IADistanceComparator.isWithinRange(mockCurrentLocation, mockTargetLocation, RANGE));
+    }
+    @Test
+    public void isWithinRangeReturnsFalseWhenDistanceIs3() throws Exception {
+        //set target
+        mockTargetLocation.setLongitude(-0.1299845104014142);
+        mockTargetLocation.setLatitude(51.52166396722949);
+        //set current
+        IALocation.Builder builder = new IALocation.Builder();
+        IALocation.Builder bLong = builder.withLongitude(-0.1299845104014142);
+        IALocation.Builder bLongAndLat = bLong.withLatitude(51.52166396722949);
+        mockCurrentLocation = bLongAndLat.build();
+        //check
+        assertFalse(IADistanceComparator.isWithinRange(mockCurrentLocation, mockTargetLocation, RANGE));
+    }
+
+}
